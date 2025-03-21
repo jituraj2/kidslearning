8f0649af3f0764afc801272edc0284736bcf967e
jituraj2 <jrajbher@gmail.com>
2025-03-21 17:27:19 +0530

    Update index.js

--- src/public/index.js
+++ src/public/index.js
@@ -1,7 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // Optional, if you have global styles
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kids Learning Game</title>
+</head>
+<body>
+    <div id="root"></div>
+</body>
+</html>
